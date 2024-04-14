c2e23295e415eb7b96e757916b76bad39d60776a
Mark Ferianec <mjf397@gmail.com>
2024-04-14 05:11:08 -0600

    Modify GeneralInformation
    Added additional states to save input. Added unfinished submit and edit
    buttons.

--- cv-application/src/components/GeneralInformation.jsx
+++ cv-application/src/components/GeneralInformation.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 
 function GeneralInformation() {
-  // const [generalInformation, setGeneralInformation] = useState({});
   const [name, setName] = useState('');
+  const [savedName, setSavedName] = useState('');
+
   const [email, setEmail] = useState('');
+  const [savedEmail, setSavedEmail] = useState('');
+
   const [phone, setPhone] = useState('');
+  const [savedPhone, setSavedPhone] = useState('');
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -18,19 +22,51 @@ function GeneralInformation() {
     setPhone(e.target.value);
   }
 
+  function submitButton(e) {
+    e.preventDefault();
+    setSavedName(name);
+    setSavedEmail(email);
+    setSavedPhone(phone);
+
+    setName('');
+    setEmail('');
+    setPhone('');
+  }
+
+  function editButton(e) {
+    e.preventDefault();
+    setName(savedName);
+    setEmail(savedEmail);
+    setPhone(savedPhone);
+  }
+
   return (
-    <>
-      <h1>General Information</h1>
-      <h2>Name</h2>
-      <input placeholder="Name" value={name} onChange={handleNameChange} />
-      <h2>Email</h2>
-      <input placeholder="Email" value={email} onChange={handleEmailChange} />
-      <h2>Phone</h2>
-      <input placeholder="Phone Number" value={phone} onChange={handlePhoneChange} />
+    <div>
       <div>
-        {name} {email} {phone}
+        <h1>General Information</h1>
+        <form>
+          <h2>Name</h2>
+          <input placeholder="Name" onChange={handleNameChange} value={name} />
+          <h2>Email</h2>
+          <input
+            placeholder="Email"
+            onChange={handleEmailChange}
+            value={email}
+          />
+          <h2>Phone</h2>
+          <input
+            placeholder="Phone Number"
+            onChange={handlePhoneChange}
+            value={phone}
+          />
+          <button onClick={editButton}>Edit</button>
+          <button onClick={submitButton}>Submit</button>
+        </form>
       </div>
-    </>
+      <div>
+        {savedName} {savedEmail} {savedPhone}
+      </div>
+    </div>
   );
 }
 
