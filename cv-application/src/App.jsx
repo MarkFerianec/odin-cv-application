2e2ed90f5d71e0a253f4f5ae17a19616da4fb2b9
Mark Ferianec <mjf397@gmail.com>
2024-04-18 14:10:13 -0600

    Modify App
    Combined multiple states into one. Deleted comments.

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -22,55 +22,12 @@ function App() {
     setGeneralInformationValues({ ...generalInformationValues, [name]: value });
   };
 
-  // Experimental
-  // function handleGeneralInformationInputChange() {
-  //   setGeneralInformationValues(['test', 'test', 'test']);
-  // }
-
-  // const handleGeneralInformationInputChange = (e) => {
-  //   const [ name, value ] = e.target;
-  //   setGeneralInformationValues({ ...generalInformationValues, [name]: value });
-  // };
-
-  // const [name, setName] = useState('');
-  // const [savedName, setSavedName] = useState('');
-  // const [savedName, setSavedName] = useState(generalInformationValues.name);
-  const [savedName, setSavedName] = useState('');
-
-  // const [email, setEmail] = useState('');
-  const [savedEmail, setSavedEmail] = useState('');
-
-  // const [phone, setPhone] = useState('');
-  const [savedPhone, setSavedPhone] = useState('');
-
-  // function handleNameChange(e) {
-  //   setName(e.target.value);
-  // }
-
-  // Mod this?
-  // function handleNameChange(e) {
-  //   setName(e.target.value);
-  // }
-
-  // function handleEmailChange(e) {
-  //   setEmail(e.target.value);
-  // }
-
-  // function handlePhoneChange(e) {
-  //   setPhone(e.target.value);
-  // }
+  const [savedGeneralInformation, setSavedGeneralInformation] = useState('');
 
   function submitButton(e) {
     e.preventDefault();
 
-    // setSavedName(name);
-    setSavedName(generalInformationValues.Name);
-    setSavedEmail(generalInformationValues.Email);
-    setSavedPhone(generalInformationValues['Phone Number']);
-    // setSavedName('test');
-    // setGeneralInformationValues(generalInformationValues.name);
-    // setSavedEmail(email);
-    // setSavedPhone(phone);
+    setSavedGeneralInformation(Object.values(generalInformationValues));
 
     toggleFormDisplay();
   }
@@ -108,54 +65,10 @@ function App() {
           submitButton={submitButton}
           editButton={editButton}
         />
-        {/* <GeneralInformation
-          formDisplay={formDisplay}
-          name={name}
-          phone={phone}
-          email={email}
-          handleNameChange={handleNameChange}
-          handleEmailChange={handleEmailChange}
-          handlePhoneChange={handlePhoneChange}
-          submitButton={submitButton}
-          editButton={editButton}
-          generalInformationValues={generalInformationValues}
-          handleGeneralInformationInputChange={
-            handleGeneralInformationInputChange
-          }
-        /> */}
-        {/* <Form
-          formDisplay={formDisplay}
-          title="General Information"
-          inputs={['Name', 'Email', 'Phone Number']}
-          handleGeneralInformationInputChange={
-            handleGeneralInformationInputChange
-          }
-          submitButton={submitButton}
-          savedName={savedName}
-          savedEmail={savedEmail}
-          savedPhone={savedPhone}
-          editButton={editButton}
-        /> */}
-        {/* <Form
-          formDisplay={formDisplay}
-          title="Educational Experience"
-          inputs={['School Name', 'Title of Study', 'Date of Study']}
-          submitButton={submitButton}
-        /> */}
-        {/* <Form
-          formDisplay={formDisplay}
-          title="Practical Experience"
-          inputs={[
-            'Company Name',
-            'Position Title',
-            'Responsibilties',
-            'Dates Worked',
-          ]}
-          submitButton={submitButton}
-        /> */}
       </div>
       <div className="CV">
-        {savedName} {savedEmail} {savedPhone}
+        {savedGeneralInformation[0]} {savedGeneralInformation[1]}{' '}
+        {savedGeneralInformation[2]}
       </div>
     </>
   );
